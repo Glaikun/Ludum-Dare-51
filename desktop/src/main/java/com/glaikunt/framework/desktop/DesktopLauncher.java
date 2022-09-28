427dad9a085aa31e41f6ee1d0cdf3cbece0fb630
Peter Bell <peterdbell@gmail.com>
2022-09-28 01:40:09 +0100

    PB | lwjgl3 upgrade

--- desktop/src/main/java/com/glaikunt/framework/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/glaikunt/framework/desktop/DesktopLauncher.java
@@ -1,20 +1,26 @@
 package com.glaikunt.framework.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.glaikunt.framework.DynamicDisplay;
 
+/**
+ * Conversion to lwjgl3
+ *  - default libgdx backend version
+ * 	- discontinuation of lwjgl3
+ * 	- better support for JREs, macOS, Linux and muti-window environments
+ * https://libgdx.com/news/2021/07/devlog-7-lwjgl3
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//		config.width = (int) (1440 * .9f);
-//		config.height = (int) (900 * .9f);
-		config.width = 1280;
-		config.height = 960;
-		config.title = "Libgdx Framework";
-		config.resizable = false;
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Libgdx Framework");
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		config.setWindowedMode(1280, 960);
+		config.setResizable(false);
 //		new LwjglApplication(new Display2D(), config);
 //		new LwjglApplication(new Display3D(), config);
-		new LwjglApplication(new DynamicDisplay(), config);
+		new Lwjgl3Application(new DynamicDisplay(), config);
 	}
 }
