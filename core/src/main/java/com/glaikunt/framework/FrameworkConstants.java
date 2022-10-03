0a3b1761bd30d6f2b4a6d347f6257bf1c17625b3
Dregron <Andymurray2007@gmail.com>
2022-10-03 19:41:48 +0100

    - Adding heat source animation

--- core/src/main/java/com/glaikunt/framework/FrameworkConstants.java
+++ core/src/main/java/com/glaikunt/framework/FrameworkConstants.java
@@ -7,4 +7,6 @@ public interface FrameworkConstants {
     Color DARK_BLUE = new Color(26f / 255f, 63 / 255f, 115f / 255f, 1);
     Color LIGHT_BLACK = new Color(54f / 255f, 68f / 255f, 54f / 255f, 1);
     Color WHITE = new Color(195 / 255f, 208f / 255f, 194f / 255f, 1);
+    Color RED = new Color(159 / 255f, 62 / 255f, 62 / 255f, 1);
+    Color ORANAGE = new Color(216 / 255f, 154 / 255f, 48 / 255f, 1);
 }
--- core/src/main/java/com/glaikunt/framework/game/map/HeatSourceActor.java
+++ core/src/main/java/com/glaikunt/framework/game/map/HeatSourceActor.java
@@ -3,23 +3,29 @@ package com.glaikunt.framework.game.map;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.glaikunt.framework.FrameworkConstants;
 import com.glaikunt.framework.application.ApplicationResources;
 import com.glaikunt.framework.application.CommonActor;
+import com.glaikunt.framework.application.TickTimer;
 import com.glaikunt.framework.cache.TextureCache;
 import com.glaikunt.framework.esc.component.animation.AnimationComponent;
 import com.glaikunt.framework.esc.system.physics.BodyComponent;
 import com.glaikunt.framework.esc.system.physics.BodyType;
+import com.glaikunt.framework.pixels.ExplodePixelActor;
 
 public class HeatSourceActor extends CommonActor {
 
     private final BodyComponent body;
     private final AnimationComponent animation;
 
+    private TickTimer pixelsTimer = new TickTimer(1f);
+
     public HeatSourceActor(ApplicationResources applicationResources, Vector2 pos) {
         super(applicationResources);
 
-        this.animation = new AnimationComponent(applicationResources.getCacheRetriever().geTextureCache(TextureCache.HEATSOURCE), 1, 1);
+        this.animation = new AnimationComponent(applicationResources.getCacheRetriever().geTextureCache(TextureCache.HEATSOURCE), 4, 1);
         this.pos.set(pos);
         this.size.set(animation.getCurrentFrame().getRegionWidth()-1, animation.getCurrentFrame().getRegionHeight()-1);
 
@@ -36,6 +42,19 @@ public class HeatSourceActor extends CommonActor {
     public void draw(Batch batch, float parentAlpha) {
 
         batch.draw(animation.getCurrentFrame(), getX(), getY(), getWidth(), getHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+
+        pixelsTimer.tick(delta);
+        if (pixelsTimer.isTimerEventReady()) {
+
+//           float xPos, float yPos, float targetXPos, float targetYPos, float width, float height, Color color, int amount, float speed) {
+            float x = MathUtils.random(getX()+5, getX() +(getWidth()));
+            float y = MathUtils.random(getY()+5, getY() +(getHeight()/2));
+            getStage().addActor(new ExplodePixelActor(getApplicationResources(), x, y, 1, 1, new Color[]{FrameworkConstants.ORANAGE, FrameworkConstants.RED}, 15, 5));
+        }
     }
 
     @Override
